eef42101c1607f25f39b6542b71b616c60b256b4
Sviatozar <svpetrenko123@gmail.com>
2021-10-03 13:59:12 +0300

    Fix gitkraken issue

--- scripts/domManipulations.js
+++ scripts/domManipulations.js
@@ -1,7 +1,7 @@
-const domManipulations = {
-    switchContentsOf: function (id1, id2) {
-        let item1 = document.getElementById(id1);
-        let item2 = document.getElementById(id2);
-        [item1.innerHTML, item2.innerHTML] = [item2.innerHTML, item1.innerHTML];
-    }
-}
+////const domManipulations = {
+////  switchContentsOf: function (id1, id2) {
+////    let item1 = document.getElementById(id1);
+////    let item2 = document.getElementById(id2);
+////    [item1.innerHTML, item2.innerHTML] = [item2.innerHTML, item1.innerHTML];
+////  },
+////};
