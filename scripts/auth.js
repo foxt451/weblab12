6feb833c0133cbb6c012d2ad3815eb29b0fb8389
Sviatozar <svpetrenko123@gmail.com>
2021-12-29 13:31:09 +0200

    Lab5

--- scripts/auth.js
+++ scripts/auth.js
@@ -7,19 +7,90 @@ const firebaseConfig = {
   messagingSenderId: '238126799846',
   appId: '1:238126799846:web:afa0690cc8fe6b7c80d427',
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const authService = firebase.auth();
 
+document.querySelector('.logoutBtn').addEventListener('click', () => {
+  authService
+    .signOut()
+    .then(() => {
+      document.querySelector('.statusInp').value = '';
+    })
+    .catch((err) => {
+      alert(`Error happened: ${err}`);
+    });
+});
+
+document.querySelector('.statusUpdBtn').addEventListener('click', () => {
+  const user = authService.currentUser;
+  if (user) {
+    const db = firebase.firestore();
+    db.collection('statuses')
+      .doc(user.uid)
+      .set({
+        status: document.querySelector('.statusInp').value,
+      })
+      .then(() => {
+        alert('Updated successfully!');
+      })
+      .catch((err) => {
+        alert(`Error happened: ${err}`);
+      });
+  }
+});
+
+function showForLoggedOut() {
+  document.querySelector('.userinfo').classList.add('collapsed');
+  document.querySelector('.logoutBtn').classList.add('collapsed');
+}
+
+function showForLoggenIn() {
+  document.querySelector('.userinfo').classList.remove('collapsed');
+  document.querySelector('.logoutBtn').classList.remove('collapsed');
+}
+
 const ui = new firebaseui.auth.AuthUI(authService);
 
-const uiConfig = {
-  // callbacks: {
-  //   signInSuccessWithAuthResult(authResult, redirectUrl) {
-  //     return true;
-  //   },
-  // },
-  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
-};
+authService.onAuthStateChanged((user) => {
+  if (user) {
+    // try get related status
+    //const token = user.getIdToken(true);
+    showForLoggenIn();
+    const uid = user.uid;
+    document.querySelector('.username').innerText = user.displayName;
+    const db = firebase.firestore();
+    db.collection('statuses')
+      .doc(uid)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          document.querySelector('.statusInp').value = doc.data().status;
+        } else {
+          document.querySelector('.statusInp').value = '';
+        }
+      })
+      .catch((err) => {
+        alert(`Error happened: ${err}`);
+      });
+  } else {
+    showForLoggedOut();
 
-ui.start('.authContainer', uiConfig);
+    const uiConfig = {
+      callbacks: {
+        signInSuccessWithAuthResult() {
+          return false;
+        },
+      },
+      signInOptions: [
+        {
+          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          requireDisplayName: true,
+        },
+      ],
+    };
+
+    ui.start('.authContainer', uiConfig);
+  }
+});
